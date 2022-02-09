0f45890e6fb8d4642eff59ce466164ae10010c2a
gustavovaznet <98855318+gustavovaznet@users.noreply.github.com>
2022-02-09 13:13:43 -0300

    Upload backend files

---
+++ server/src/controllers/ConnectionsController.ts
@@ -0,0 +1,23 @@
+//CONNECTION CONTROLLER
+
+//IMPORTING
+import { Request, Response } from 'express';
+import db from '../database/connection';
+
+//CONNECTIONS CONTROLLER
+export default class ConnectionsController {
+    //INDEX
+    async index(request: Request,response: Response){
+        const totalConnections = await db('connections').count('* as total');
+        const { total } = totalConnections[0];
+        return response.json({total});
+    }
+    //CREATE
+    async create(request: Request,response: Response){
+        const { user_id } = request.body;
+        await db('connections').insert({
+            user_id,
+        });
+        return response.status(201).send();
+    }
+}
---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+//SERVER
+
+//IMPORTING
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+//PORT
+app.listen(3333);
